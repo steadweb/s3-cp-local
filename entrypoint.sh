ba0f13439bc85af3b8d5c776543f90ff0dd54151
Luke Steadman <ljsteadman@gmail.com>
2020-09-03 23:01:20 +0100

    Use region insteadUse region instead

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 cp s3://${AWS_S3_BUCKET}/${SOURCE} --region ${AWS_REGION} ${DESTINATION}
+aws s3 cp s3://${AWS_S3_BUCKET}/${SOURCE} --region ${REGION} ${DESTINATION}
 
 rm -rf ~/.aws
 
